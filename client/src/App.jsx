a02e741a09a7f7f2bc14a5231addcf225331c82e
franco2730 <franco.rosales2130@gmail.com>
2023-09-02 18:36:53 -0300

    PI-videoGames

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import Home from './Views/Home';
 import Form from './Views/Form';
 import Landing from './Views/Form';
 import Detail from './Views/Form';
-import {  }
+import { Routes, Route, useLocation } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 
 function App() {
+
+  const location = useLocation();
 
   return (
     <div className="App">
