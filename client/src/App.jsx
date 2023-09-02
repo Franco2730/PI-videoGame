46d5fd17d4adc1f49fc7d868d7b4c19d98cc4482
franco2730 <franco.rosales2130@gmail.com>
2023-09-02 13:27:40 -0300

    PI-videoGames

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
+import Home from './Views/Home';
+import Form from './Views/Form';
+import Landing from './Views/Form';
+import Detail from './Views/Form';
+import {  }
+
 
 function App() {
 
   return (
     <div className="App">
-      <>
-
-      </>
+     <Home/>
+     <Form/>
+     <Landing/>
+     <Detail/>
     </div>
   );
 }
 
 export default App;
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Home from './Views/Home.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Home />
   </React.StrictMode>,
 )
-// Tener en cuenta la imagen, cargar tareas en: https://trello.com/b/350TgZZ0/proyecto-individual
