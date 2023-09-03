b61a9c4640be72bdae3d8e1ecc02a099a9a961cd
franco2730 <franco.rosales2130@gmail.com>
2023-09-03 17:27:16 -0300

    PI-videoGames

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,19 @@
-import React from 'react' //Video 02:11
-import ReactDOM from 'react-router-dom'
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './Redux/store'
-
-
-
+import store from './redux/store'
+import App from './App.jsx'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        
+         <App />
+      
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 )
